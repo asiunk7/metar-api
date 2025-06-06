cc61b12d32ecb7ceb77d06b40f7788553a832028
asiunk7 <104195267+asiunk7@users.noreply.github.com>
2025-06-06 23:46:25 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import requests
+import os
 
 app = Flask(__name__)
 
@@ -19,3 +20,7 @@ def get_metar():
             return jsonify({"metar": "No METAR found"}), 404
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # <= fix disini bro
+    app.run(host="0.0.0.0", port=port)
